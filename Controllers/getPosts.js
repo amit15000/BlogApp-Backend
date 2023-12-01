c21c8bd7ea94ec06fef3cab71e98e01163aaaca2
Amit Kumar <15000amitkumar@gmail.com>
2023-12-01 11:42:36 +0530

    createPost and getPost

---
+++ Controllers/getPosts.js
@@ -0,0 +1,25 @@
+const Post = require("../Models/Post");
+
+exports.getPosts = async (req, res) => {
+  try {
+    const item = await Post.find({});
+    if (!item) {
+      return res.status(404).json({
+        success: false,
+        data: item,
+        message: "No item present",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: item,
+      message: "Items fetched successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      data: err.message,
+      message: "Server internal error",
+    });
+  }
+};
--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -2,7 +2,9 @@
 const express = require("express");
 const router = express.Router();
 const { createPost } = require("../Controllers/createPost");
+const { getPosts } = require("../Controllers/getPosts.js");
 
 //router was writeen in function and that function was never been called.
 router.post("/createPost", createPost);
+router.get("/getPosts", getPosts);
 module.exports = router;
